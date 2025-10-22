484bb286ea192faf405369598c116f3473eb4139
LambdaCalc <mudriyjo@gmail.com>
2025-10-22 10:41:00 +0300

    prepare base get hello world service

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| hello_world()));
+    let listener = tokio::net::TcpListener::bind("localhost:8080")
+        .await
+        .unwrap();
+
+    axum::serve(listener, app).await.unwrap()
+}
+
+async fn hello_world() -> String {
+    "Hello, world!".to_string()
 }
